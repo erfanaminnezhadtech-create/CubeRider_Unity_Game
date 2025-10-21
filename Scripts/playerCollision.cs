74632bb39ca04a2619d85f4c69f43d27ffe70cf3
Erfan Aminnezhad <erfanaminnezhadtech@gmail.com>
2025-10-21 21:32:33 +0330

    first commit

---
+++ Scripts/playerCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCollision : MonoBehaviour {
+
+	public PlayerMovement movement;
+
+	void OnCollisionEnter (Collision collisionInfo)
+	{
+		if (collisionInfo.collider.tag == "Obstacle") 
+		{
+			Debug.Log("Hi");
+			movement.enabled = false;
+			FindObjectOfType<GameManager> ().EndGame ();
+		}
+	}
+
+}
